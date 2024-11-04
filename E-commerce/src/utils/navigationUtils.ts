e5d9db44a3f9c3120c17c5d2d31379eff8969451
Карпова Ксения <ksenia.karpova.4@mail.ru>
2024-11-04 19:00:49 +0300

    gitignore

--- E-commerce/src/utils/navigationUtils.ts
+++
@@ -1,9 +0,0 @@
-// navigationUtils.ts
-import { useNavigate } from 'react-router-dom';
-import { ProductI } from 'pages/HomePage/HomePage';
-
-export const handleCardClick = (product: ProductI, products: ProductI[], navigate: (path: string, options?: any) => void) => {
-  navigate(`/product/${product.id}`, {
-    state: { product, products },
-  });
-};
